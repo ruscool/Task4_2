f3119b2ee85daecc65e55c72e95b5c9b202a70be
RuslanR <ruscool79@gmail.com>
2024-04-12 21:22:19 +0300

    add Task4_2

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // Задание 4.2.1
+        System.out.println("Задание 4.2.1");
+        UniqueNumberCounter.subMain();
+        // Задание 4.2.2
+        System.out.println("\nЗадание 4.2.2");
+        UniqueWordsCounter.subMain();
+    }
+}
